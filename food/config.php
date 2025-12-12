632824176c214ebfce0799c47c34d463fcd9c2e6
Khalid <99068092+Khalid7kmi@users.noreply.github.com>
2025-12-12 18:35:48 +0300

    Set timezone for database connection

--- food/config.php
+++ food/config.php
@@ -5,6 +5,7 @@
     $password = "**********";
     $db = "if0_39836550_food_ordering_system";
 
+date_default_timezone_set('Asia/Riyadh');
 
    $conn = new mysqli($host, $username, $password, $db);
 
@@ -12,6 +13,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$conn->query("SET time_zone = '+03:00'");
 
     
     ?>
